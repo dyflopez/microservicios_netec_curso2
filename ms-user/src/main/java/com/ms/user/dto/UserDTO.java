7e0664ef23ba924bb530d60d088c10461d937abe
dflorezPcNetec <daniel0223@hotmail.es>
2023-11-14 18:37:57 -0600

    creacion userDTO

---
+++ ms-user/src/main/java/com/ms/user/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.ms.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+
+    private String document;
+
+    private String name;
+
+    private String email;
+
+    private String information;
+
+}
