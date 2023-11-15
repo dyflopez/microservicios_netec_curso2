53e45f2fc2c85a58f74e9c0d52f6f9290f5f6316
dflorezPcNetec <daniel0223@hotmail.es>
2023-11-14 19:03:00 -0600

    documentacion API

--- ms-user/src/main/java/com/ms/user/controller/v1/docs/UserDoc.java
+++ ms-user/src/main/java/com/ms/user/controller/v1/docs/UserDoc.java
@@ -1,5 +1,28 @@
 package com.ms.user.controller.v1.docs;
 
 
+import com.ms.user.dto.UserDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "user controller",description = "API Exposed for CRUD operations on user")
 public interface UserDoc {
+
+
+    @Operation(summary =  "create user",
+    description =  "This operation is for creating a user")
+    @ApiResponses(
+            value ={
+                    @ApiResponse(responseCode = "201",
+                    description = "user created",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+            }
+    )
+    ResponseEntity create(@RequestBody UserDTO userDTO);
 }
