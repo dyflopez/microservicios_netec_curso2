4650478d0593d59e42171bd63fcc960af02a3c46
dflorezPcNetec <daniel0223@hotmail.es>
2023-11-23 17:53:36 -0600

    creacion eureka

--- proyecto/hotel-ms/src/main/java/com/ms/hotel/hotel/HotelMsApplication.java
+++ proyecto/hotel-ms/src/main/java/com/ms/hotel/hotel/HotelMsApplication.java
@@ -2,8 +2,10 @@ package com.ms.hotel.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HotelMsApplication {
 
 	public static void main(String[] args) {
