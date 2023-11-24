e4cac9598bf54ff70648c1a2253b073e7d3248ea
dflorezPcNetec <daniel0223@hotmail.es>
2023-11-23 19:03:13 -0600

    creacion eureka

--- proyecto/ms-user/src/main/java/com/ms/user/dto/UserRankingDTO.java
+++ proyecto/ms-user/src/main/java/com/ms/user/dto/UserRankingDTO.java
@@ -1,4 +1,25 @@
 package com.ms.user.dto;
 
+
+import lombok.*;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRankingDTO {
+
+    private String document;
+
+    private String name;
+
+    private String email;
+
+    private String information;
+
+    private List<RankingDTO> ranking;
+
 }
--- proyecto/ms-user/src/main/java/com/ms/user/service/impl/ServiceImpl.java
+++ proyecto/ms-user/src/main/java/com/ms/user/service/impl/ServiceImpl.java
@@ -1,7 +1,11 @@
 package com.ms.user.service.impl;
 
 
+import com.ms.user.conf.RestTemplateConfig;
+import com.ms.user.dto.HotelDTO;
+import com.ms.user.dto.RankingDTO;
 import com.ms.user.dto.UserDTO;
+import com.ms.user.dto.UserRankingDTO;
 import com.ms.user.exception.MyHandleException;
 import com.ms.user.mapper.UserMapper;
 import com.ms.user.model.UserEntity;
@@ -11,12 +15,19 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class ServiceImpl implements IUserService {
+
+    private final RestTemplate restTemplate;
 
 
     private  final UserRepository userRepository;
@@ -44,7 +55,43 @@ public class ServiceImpl implements IUserService {
         var user=  this
                 .userRepository
                 .findById(id).orElseThrow(()-> new MyHandleException("User does not exist"));
-        return ResponseEntity.ok(user);
+
+        UserRankingDTO userRankingDTO =new UserRankingDTO();
+
+        userRankingDTO.setName(user.getName());
+        userRankingDTO.setEmail(user.getEmail());
+        userRankingDTO.setInformation(user.getInformation());
+        userRankingDTO.setDocument(user.getDocument());
+
+/*
+        List<RankingDTO>  rankingDTOS = this
+                .restTemplate
+                .getForObject("http://localhost:8083/rankings/user/"+user.getId(), ArrayList.class);
+*/
+
+        RankingDTO[] rankingsVe= this.restTemplate.getForObject("http://MS-RANKING/rankings/user/"+user.getId(),RankingDTO[].class);
+
+        var rankings = Arrays.stream(rankingsVe).collect(Collectors.toList());
+
+
+        rankings.stream().map(rankin->{
+
+            ResponseEntity<HotelDTO> hotelReposnse  = this
+                    .restTemplate
+                    .getForEntity("http://MS-HOTEL/hotel/"+rankin.getHotelId(),HotelDTO.class);
+
+            var hotel    = hotelReposnse.getBody();
+
+                    rankin.setHotel(hotel);
+            return rankin;
+            }
+        ).collect(Collectors.toList());
+
+        userRankingDTO.setRanking(rankings);
+
+
+
+        return ResponseEntity.ok(userRankingDTO);
     }
 
 
